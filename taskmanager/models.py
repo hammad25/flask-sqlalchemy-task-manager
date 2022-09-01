6ddbe4eb5bd616fb58ca9ab336ffb809cf0ffc35
Hammad <hammad2091504@hotmail.com>
2022-09-01 11:54:27 +0000

    Add add_task.html and route

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -15,10 +15,10 @@ class Task(db.Model):
     #schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
     task_name = db.Column(db.String(50), unique=True, nullable=False)
-    task_description = db.Column(db.Text, unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    catergory_id= db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    category_id= db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
 
 
     def __repr__(self):
